687467e6b2da87884f0c1e8a715b590b85f198eb
unknown <kenrickng90@gmail.com>
2020-01-29 22:21:59 +0800

    final

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-n = int(input())
 arr = []
-for _ in range(n):
-    arr_item = int(input())
-    arr.append(arr_item)
+for i in range(int(input())):
+    arr.append(int(input()))
+# candy left to right, candy right to left, and candy array
+candy_lr = [1]*(len(arr))
+candy_rl = [1]*(len(arr))
+candy = []
 
+# traverses from left to right and assigns candy to students
+for i in range(1,len(arr)):
+    if arr[i]>arr[i-1]:
+        candy_lr[i] = candy_lr[i-1]+1
+
+# traverses from right to left and assigns candy to students
+for i in range(len(arr)-2,-1,-1):
+    if arr[i]>arr[i+1]:
+        candy_rl[i] = candy_rl[i+1]+1
+
+#calculates the total candy needed
+for i in range(0,len(arr)):
+    candy.append(max(candy_lr[i],candy_rl[i]))
+print(sum(candy))
